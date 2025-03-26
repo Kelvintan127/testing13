5d4b0f87a10632aca2984e6a865a9a4d85592444
Kelvintan127 <kelvin.tan@binus.ac.id>
2025-03-26 18:35:46 +0700

    tes

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/will-you-be-my-valentine",  // Update base URL to match your GitHub Pages path
+  base: "/",  // Update base URL to match your GitHub Pages path
   plugins: [react()],
   server: {
     headers: {
