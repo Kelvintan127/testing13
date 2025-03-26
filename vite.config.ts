c6baec0ee07997497c9d75a81728585846fb55a6
Kelvintan127 <kelvin.tan@binus.ac.id>
2025-03-26 17:12:14 +0700

    add api

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/will-you-be-my-valentine",
+  base: "./",
   plugins: [react()],
   resolve: {
     alias: {
